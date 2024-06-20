f47534a98658bfccf9c022c5385f921e47704fe0
Budi <budimanfajarf@gmail.com>
2024-06-20 12:01:09 +0700

    feat: scrap provinces & cities *wip

--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-console.log("Hello via Bun!");
+import axios from 'axios';
+import { Province } from './class/province';
+import { City } from './class/city';
+
+const axiosInstance = axios.create({
+  baseURL: 'https://sipedas.pertanian.go.id',
+});
+
+const year = '2024';
+
+const { data: provincesData } = await axiosInstance.get('/api/wilayah/list_pro', {
+  params: {
+    thn: year,
+  },
+});
+
+for (const pKey in provincesData) {
+  const province = new Province({ id: 1, key: pKey, name: provincesData[pKey] });
+
+  console.log({ province });
+
+  const { data: cities } = await axiosInstance.get('/api/wilayah/list_kab', {
+    params: {
+      thn: year,
+      pro: province.code,
+    },
+  });
+
+  for (const cKey in cities) {
+    const city = new City({ id: 1, key: cKey, name: cities[cKey], province });
+
+    console.log({ city });
+  }
+}
