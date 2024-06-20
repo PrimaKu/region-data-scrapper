b007423b6916fba5607fb355b071e12766a4a8b4
Budi <budimanfajarf@gmail.com>
2024-06-20 13:01:02 +0700

    feat: save provinces & cities to sql file

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { Province } from './class/province';
 import { City } from './class/city';
+import fs from 'fs';
+import path from 'path';
 
 const axiosInstance = axios.create({
   baseURL: 'https://sipedas.pertanian.go.id',
@@ -63,5 +65,34 @@ const getCities = async ({
   return cities;
 };
 
+const saveProvincesToSQLFile = (provinces: Province[]) => {
+  const filePath = path.join(__dirname, 'result', 'Province.sql');
+  const sqlStatements = provinces
+    .map(
+      (province) =>
+        `INSERT INTO Province (id, code, name) VALUES (${province.id}, '${province.code}', '${province.name}');`
+    )
+    .join('\n');
+
+  fs.writeFileSync(filePath, sqlStatements);
+  console.log(`Provinces saved to ${filePath}`);
+};
+
+const saveCitiesToSQLFile = (cities: City[]) => {
+  const filePath = path.join(__dirname, 'result', 'City.sql');
+  const sqlStatements = cities
+    .map(
+      (city) =>
+        `INSERT INTO City (id, code, provinceId, name) VALUES (${city.id}, '${city.code}', '${city.provinceId}', ${city.name});`
+    )
+    .join('\n');
+
+  fs.writeFileSync(filePath, sqlStatements);
+  console.log(`Cities saved to ${filePath}`);
+};
+
 const provinces = await getProvinces();
 const cities = await getCities({ provinces });
+
+saveProvincesToSQLFile(provinces);
+saveCitiesToSQLFile(cities);
