8a41e732b598fb904161e780bc58b4f2d5408d21
Budi <budimanfajarf@gmail.com>
2024-06-20 12:40:16 +0700

    feat: scrapp provincies & cities

--- index.ts
+++ index.ts
@@ -6,29 +6,62 @@ const axiosInstance = axios.create({
   baseURL: 'https://sipedas.pertanian.go.id',
 });
 
-const year = '2024';
+const getProvinces = async ({ year = 2024, timeoutBeforeReq = 1000 } = {}) => {
+  const provinces: Province[] = [];
+  let provinceId = 1;
 
-const { data: provincesData } = await axiosInstance.get('/api/wilayah/list_pro', {
-  params: {
-    thn: year,
-  },
-});
+  console.log('Getting provinces...');
+  await new Promise((resolve) => setTimeout(resolve, timeoutBeforeReq)); // to prevent too many request
 
-for (const pKey in provincesData) {
-  const province = new Province({ id: 1, key: pKey, name: provincesData[pKey] });
-
-  console.log({ province });
-
-  const { data: cities } = await axiosInstance.get('/api/wilayah/list_kab', {
+  const { data } = await axiosInstance.get('/api/wilayah/list_pro', {
     params: {
       thn: year,
-      pro: province.code,
     },
   });
 
-  for (const cKey in cities) {
-    const city = new City({ id: 1, key: cKey, name: cities[cKey], province });
-
-    console.log({ city });
+  for (const key in data) {
+    const province = new Province({ id: provinceId++, key, name: data[key] });
+    provinces.push(province);
+    // console.log({ province });
+    console.log(`ID ${province.id} | Code ${province.code} | ${province.name}`);
   }
-}
+
+  return provinces;
+};
+
+const getCities = async ({
+  provinces,
+  year = 2024,
+  timeoutBeforeReq = 1000,
+}: {
+  provinces: Province[];
+  year?: number;
+  timeoutBeforeReq?: number;
+}) => {
+  const cities: City[] = [];
+  let cityId = 1;
+
+  for (const province of provinces) {
+    console.log(`Getting cities of ${province.name}...`);
+    await new Promise((resolve) => setTimeout(resolve, timeoutBeforeReq)); // to prevent too many request
+
+    const { data } = await axiosInstance.get('/api/wilayah/list_kab', {
+      params: {
+        thn: year,
+        pro: province.code,
+      },
+    });
+
+    for (const key in data) {
+      const city = new City({ id: cityId++, key, name: data[key], province });
+      cities.push(city);
+      // console.log({ city });
+      console.log(`ID ${city.id} | Code ${city.code} | ${city.name}`);
+    }
+  }
+
+  return cities;
+};
+
+const provinces = await getProvinces();
+const cities = await getCities({ provinces });
